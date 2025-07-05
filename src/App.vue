87803107324a13af95161274ab8675fe3526d4ad
Kerly Sarrias <kerlysarrias011@gmail.com>
2025-07-05 12:39:38 -0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>
+        Legends
+      </h1>
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
